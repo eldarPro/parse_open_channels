d29706b70ee3b0173c55b06118f43bc4589c9bf0
Eldar <eldar@telega.in>
2024-01-30 04:29:46 +0300

    fix

--- app/workers/update_posts_worker.rb
+++ app/workers/update_posts_worker.rb
@@ -19,36 +19,32 @@ class UpdatePostsWorker
          ActiveRecord::Base.connection.quote(val[1]),
          ActiveRecord::Base.connection.quote(val[2]),
          "#{ActiveRecord::Base.connection.quote(val[3].to_json)}",
-         "#{ActiveRecord::Base.connection.quote([{views: val[2], updated_at: val[-1]}].to_json)}",
          ActiveRecord::Base.connection.quote(val[4]),
          ActiveRecord::Base.connection.quote(val[5]),
-         ActiveRecord::Base.connection.quote(val[6]),
          ActiveRecord::Base.connection.quote(val[7]),
          ActiveRecord::Base.connection.quote(val[8]),
          ActiveRecord::Base.connection.quote(val[9]),
          ActiveRecord::Base.connection.quote(val[10]),
-         ActiveRecord::Base.connection.quote('true'),
          ActiveRecord::Base.connection.quote(val[11]),
          ActiveRecord::Base.connection.quote(val[12]),
-         ActiveRecord::Base.connection.quote(val[13])].join(', ')}, 
-         NOW(),
-         NOW())"
+         ActiveRecord::Base.connection.quote(val[13])].join(', ')})"
       end.join(', ')
 
-      MainDbRecord.connection.execute("UPDATE posts AS p
-        SET views = views, 
-        links = links::jsonb,
-        has_photo = has_photo,
-        has_video = has_video,
-        next_post_at = CAST(next_post_at AS TIMESTAMP WITH TIME ZONE),
-        html = html,
-        is_repost = is_repost,
-        feed_hours = feed_hours,
-        top_hours = top_hours,
-        last_parsed_at = CAST(last_parsed_at AS TIMESTAMP WITH TIME ZONE),
-        statistic = jsonb_insert(p.statistic, '{-1}', jsonb_build_object('views', (views::int - COALESCE(p.views, 0)::int), 'updated_at', last_parsed_at), true)
-        FROM VALUES #{update_values}
-      ")
+      MainDbRecord.connection.execute("UPDATE posts AS p SET 
+        views = d.views, 
+        links = d.links::jsonb,
+        has_photo = d.has_photo,
+        has_video = d.has_video,
+        next_post_at = CAST(d.next_post_at AS TIMESTAMP WITH TIME ZONE),
+        html = d.html,
+        is_repost = d.is_repost,
+        feed_hours = d.feed_hours,
+        top_hours = d.top_hours,
+        last_parsed_at = CAST(d.parsed_at AS TIMESTAMP WITH TIME ZONE),
+        statistic = jsonb_insert(p.statistic, '{-1}', jsonb_build_object('views', (d.views::int - COALESCE(p.views, 0)::int), 'updated_at', last_parsed_at), true)
+        FROM (VALUES #{update_values}) AS 
+        d(link, tg_post_id, views, links, has_photo, has_video, next_post_at, html, is_repost, channel_id, feed_hours, top_hours, parsed_at)
+        WHERE p.channel_id = d.channel_id AND p.tg_id = d.tg_post_id;")
 
       Redis0.ltrim('update_posts_data', 3000, -1)
     end
