3a3947548fd2b2f5021fb1f0d75ccb0304b016d0
Eldar <eldar@telega.in>
2024-02-08 12:39:45 +0300

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,15 @@
 
 $(document).ready(function(){
 				
-	$(document).on('click', '.set_theme', function(){
+	$(document).on('change', '.set_theme', function(){
 		var data = { theme_id: $(this).val(), channel_id: $(this).data('channel_id') }
+
+		if($(this).val().length) {
+			$(this).addClass('border-[2px] border-green-500')
+		} else {
+			$(this).removeClass('border-[2px] border-green-500')
+		}
+		
 
 		$.get('freelancers/set_theme', data, function(data) {
 			if(data.success) $('.comp_row').text(data.count_complete)
