1d8f96e5ec2d35c4f04dc93facb7713d37ae46c5
Eldar <eldar@telega.in>
2024-02-15 16:26:33 +0300

    fix

--- app/workers/link_check_worker.rb
+++ app/workers/link_check_worker.rb
@@ -5,14 +5,14 @@ class LinkCheckWorker
   def perform(fr_id, link)
     link = "https://t.me/#{link}"
     doc = SendRequest.new(link, proxy: true).call
-    return if doc&.css('.tgme_page_extra')&.blank?
+    return if doc&.css('.tgme_page_extra')&.present?
     Redis0.rpush('empty_link_ids', fr_id)
   end
 end
 
 
-# FreelancerThemeTie.includes(:channel).find_each do |i|
-#   channel = i.channel
-#   link = channel.name || channel.joinchat
-#   LinkCheckWorker.perform_async(i.id, link)
-# end
+FreelancerThemeTie.includes(:channel).find_each do |i|
+  channel = i.channel
+  link = channel.name || channel.joinchat
+  LinkCheckWorker.perform_async(i.id, link)
+end
