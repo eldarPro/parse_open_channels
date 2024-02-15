94915253b8f9a9baaea704dc35345cf785c4b7f9
Eldar <eldar@telega.in>
2024-02-15 16:13:18 +0300

    fix

--- app/workers/link_check_worker.rb
+++ app/workers/link_check_worker.rb
@@ -3,7 +3,7 @@ class LinkCheckWorker
   sidekiq_options queue: :default
   
   def perform(fr_id, link)
-    link = "https://t.me/bankrollo"
+    link = "https://t.me/#{link}"
     doc = SendRequest.new(link, proxy: true).call
     return if doc&.css('.tgme_page_extra')&.present?
     Redis0.rpush('empty_link_ids', fr_id)
@@ -11,3 +11,8 @@ class LinkCheckWorker
 end
 
 
+# FreelancerThemeTie.includes(:channel).find_each do |i|
+#   channel = i.channel
+#   link = channel.name || channel.joinchat
+#   LinkCheckWorker.perform_async(i.id, link)
+# end
