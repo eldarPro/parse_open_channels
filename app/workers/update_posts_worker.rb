e3d06cf3299bb38e47b35e055e545572b16a1358
Eldar <eldar@telega.in>
2024-03-18 16:08:55 +0300

    fix

--- app/workers/update_posts_worker.rb
+++ app/workers/update_posts_worker.rb
@@ -46,17 +46,10 @@ class UpdatePostsWorker
         WHERE p.tg_id = d.tg_id AND p.channel_id = d.channel_id RETURNING id").to_a
 
       create_post_stats_values = []
-      create_post_infos_values = []
 
       posts_data.each do |i|
         # Сollection ChannelPostStat data
         create_post_stats_values << "(#{[i['id'], val[2]].join(', ')}, NOW())"
-        # Сollection ChannelPostInfo data
-
-        text  = TextFromHtml.new(val[8]).call # Берем только текст из html-кода 
-        text  = ActiveRecord::Base.connection.quote(text)
-        links = "#{ActiveRecord::Base.connection.quote(val[3].to_json)}"
-        create_post_infos_values << "(#{[i['id'], text, links].join(', ')}, NOW())"
       end
 
       # Create ChannelPostStat
@@ -66,16 +59,6 @@ class UpdatePostsWorker
         SELECT td.channel_post_id, td.views, td.created_at
         FROM temp_data AS td
         LEFT JOIN channel_post_stats ps ON td.channel_post_id = ps.channel_post_id AND td.views = ps.views
-        WHERE ps.channel_post_id IS NULL")
-        ActiveRecord::Base.connection.execute("DROP TABLE temp_data")
-
-      # Create ChannelPostInfo
-      ActiveRecord::Base.connection.execute("CREATE TEMPORARY TABLE temp_data (channel_post_id INTEGER, text TEXT, links TEXT[], created_at TIMESTAMP)")
-      ActiveRecord::Base.connection.execute("INSERT INTO temp_data (channel_post_id, text, links, created_at) VALUES #{create_post_infos_values.join(', ')}")
-      res = ActiveRecord::Base.connection.execute("INSERT INTO channel_post_infos (channel_post_id, text, links, created_at)
-        SELECT td.channel_post_id, td.text, td.links, td.created_at
-        FROM temp_data AS td
-        LEFT JOIN channel_post_infos ps ON td.channel_post_id = ps.channel_post_id AND td.text = ps.text AND td.links = ps.links
         WHERE ps.channel_post_id IS NULL")
         ActiveRecord::Base.connection.execute("DROP TABLE temp_data")
 
